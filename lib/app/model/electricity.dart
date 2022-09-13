61cfa88c5ab49f803fa525fe74b1a09ba2f21618
imamhossain94 <imamagun94@gmail.com>
2022-09-13 23:21:30 +0600

    feat: models are updated

--- lib/app/model/electricity.dart
+++ lib/app/model/electricity.dart
@@ -1,65 +1,47 @@
 
 import 'package:hive/hive.dart';
+import 'package:rent_manager/app/model/payment_record.dart';
 
 import 'meter_type.dart';
+import 'who_will_pay.dart';
 
 part 'electricity.g.dart';
 
 @HiveType(typeId: 0)
-class Electricity extends HiveObject {
+class ElectricityMainMeter extends HiveObject {
   @HiveField(0)
   String id;
   @HiveField(1)
-  String name;
+  String meterSerialNo;
   @HiveField(2)
-  double meterSerialNo;
+  double meterLabel;
   @HiveField(3)
   MeterType meterType;
   @HiveField(4)
-  double numberOfBalconies;
-  @HiveField(5)
-  double waterMeterId;
-  @HiveField(6)
-  double gasMeterId;
-  @HiveField(7)
-  double electricityMeterId;
-  @HiveField(8)
-  double flatOnFloorNumber;
+  WhoWillPay whoWillPay;
 
-  Flat({
+  ElectricityMainMeter({
     required this.id,
-    required this.name,
-    required this.numberOfRooms,
-    required this.numberOfBathrooms,
-    required this.numberOfBalconies,
-    required this.waterMeterId,
-    required this.gasMeterId,
-    required this.electricityMeterId,
-    required this.flatOnFloorNumber
+    required this.meterSerialNo,
+    required this.meterLabel,
+    required this.meterType,
+    required this.whoWillPay,
   });
 
-  Flat.fromJson(Map<String, dynamic> json)
+  ElectricityMainMeter.fromJson(Map<String, dynamic> json)
       : id = json['id'],
-        name = json['name'],
-        numberOfRooms = json['numberOfRooms'],
-        numberOfBathrooms = json['numberOfBathrooms'],
-        numberOfBalconies = json['numberOfBalconies'],
-        waterMeterId = json['waterMeterId'],
-        gasMeterId = json['gasMeterId'],
-        electricityMeterId = json['electricityMeterId'],
-        flatOnFloorNumber = json['flatOnFloorNumber'];
+        meterSerialNo = json['meterSerialNo'],
+        meterLabel = json['meterLabel'],
+        meterType = json['meterType'],
+        whoWillPay = json['whoWillPay'];
 
   Map<String, dynamic> toJson() {
     return {
       'id': id,
-      'name': name,
-      'numberOfRooms': numberOfRooms,
-      'numberOfBathrooms': numberOfBathrooms,
-      'numberOfBalconies': numberOfBalconies,
-      'waterMeterId': waterMeterId,
-      'gasMeterId': gasMeterId,
-      'electricityMeterId': electricityMeterId,
-      'flatOnFloorNumber': flatOnFloorNumber
+      'meterSerialNo': meterSerialNo,
+      'meterLabel': meterLabel,
+      'meterType': meterType,
+      'whoWillPay': whoWillPay
     };
   }
 }
